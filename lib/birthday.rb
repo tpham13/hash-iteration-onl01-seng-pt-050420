5a63545550d410ce4a44b1ce3007ba5ac7542583
Thanh Pham <tpham0123@gmail.com>
2020-05-16 18:02:38 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,7 +15,7 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if age > 1 
+    if age < 12 
       puts puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end 
   end 
